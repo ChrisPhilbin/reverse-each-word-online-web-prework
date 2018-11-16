0534c8aa5cf5f142948bf94414ecb67894dc1cae
fortunate-database-4943 <chris@chrisphilbin.net>
2018-11-16 18:18:55 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,9 +7,10 @@ def reverse_each_word(string)
   return reversed_array.join(" ")
 end
 
-def reverse_each_word(string)
-  new_array = string.split(" ")
-  new_array.collect do |word|
-    word.reverse
-  end
-end
+# def reverse_each_word(string)
+#  new_array = string.split(" ")
+#  new_array.collect do |word|
+#    word.reverse
+#  end
+#end
+#
